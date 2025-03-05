7a5d584cc00bb65e52ef77e46e14d62710d17675
Mohsen Moeinfar <mr.mohsen.moienfar@gmail.com>
2025-03-05 22:52:22 +0330

    starting project

---
+++ front_project/src/App.jsx
@@ -0,0 +1,87 @@
+import { Typography, Box } from "@mui/material";
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "linear-gradient(135deg, #021524, #023, #034, #056)",
+        backgroundSize: "400% 400%",
+        animation: "gradientAnimation 10s ease infinite",
+        color: "white",
+        textAlign: "center",
+        overflow: "hidden",
+      }}
+    >
+      <motion.div
+        initial={{ opacity: 0, y: 100, scale: 0.8 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 1.2, ease: "easeOut" }}
+      >
+        <Typography variant="h3" component="h1" sx={{ color: "#00e5ff" }} gutterBottom>
+        Tabriz Injector Presents
+        </Typography>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.5, ease: "easeOut", delay: 0.8 }}
+      >
+        <Typography variant="h5" component="h2" sx={{ color: "#b0bec5" }}>
+        Main Page - Still in Early Stage
+        </Typography>
+      </motion.div>
+    </Box>
+  );
+}
+
+export default App;
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
